e8c3abe10429e559fca21ed4aaafabe8fec683fa
Fergus Miles Ampuan <fergus.ampuan@gmail.com>
2024-08-17 18:52:31 +0800

    feat: Add rate limiting for book reviews
    This commit adds rate limiting for book reviews by implementing the `throttle` middleware in the `books.reviews` resource route. The rate limit is set to 3 reviews per hour for each user or IP address. This helps prevent abuse and ensures fair usage of the review feature.

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@ Route::get('/', function () {
 
 Route::resource('books',BookController::class)->only(['index', 'show']);
 
-Route::resource('books.reviews', ReviewController::class)->scoped(['review'=> 'book'])->only([ 'create','store']);
+Route::resource('books.reviews', ReviewController::class)->scoped(['review'=> 'book'])->only([ 'create','store'])->middleware(['throttle:reviews']);;
